859d49a7bfeb82d1ec656afc84e5175bed780f05
Vaishsalunke <vaishnavi.salunke@saama.com>
2022-06-07 11:49:49 +0530

    OE changes

--- Release 9/Opthalmological Examination - Listing _ new script.sql
+++ Release 9/Opthalmological Examination - Listing _ new script.sql
@@ -8,9 +8,6 @@ CREATE SCHEMA IF NOT EXISTS "ctable_listing";
 drop table if exists "ctable_listing"."cTable_oe";
 
 create table "ctable_listing"."cTable_oe" as
-
-
-
 with oe_data as (
 select
 "project" "Project",
@@ -19,19 +16,15 @@ select
 "Subject" "Subject",
 "RecordId",
 "OEPERF" ,
-"FolderName" ,
+"InstanceName" ,
 "OEDAT"::date  ,
 "OEABN" ,
 "Examination",
+"SLBL_DESC",
+"RSLBR_DESC"
 
-case when "Examination" = 'External Ocular Examination' then  "OEL_DESC"
-when "Examination" = 'Routine Slit Lamp Biomicroscopy' then "RSLBL_DESC"
-when "Examination" =  'Direct/Indirect Fundoscopy' then "FUND_LEDESC"
-end as "Result_Left_Eye",
-case when "Examination" = 'External Ocular Examination' then "OER_DESC"
-when "Examination" = 'Routine Slit Lamp Biomicroscopy' then "RSLBR_DESC"
-when "Examination" = 'Direct/Indirect Fundoscopy' then  "FUND_REDESC"
-end as "Result_Right_Eye"
+
+
 
 from(
 select
@@ -41,57 +34,50 @@ select
 "Subject",
 "RecordId",
 "OEPERF" ,
-"FolderName" ,
+"InstanceName" ,
 "OEDAT"::date  ,
 "OEABN",
 "OEL_DESC",
 "RSLBL_DESC",
 "FUND_LEDESC",
 "OER_DESC",
-"RSLBR_DESC",
 "FUND_REDESC",
-case when  "OEL_DESC" <>'' then  'External Ocular Examination'
-when "RSLBL_DESC" <>'' then  'Routine Slit Lamp Biomicroscopy'
-when  "FUND_LEDESC" <>'' then 'Direct/Indirect Fundoscopy'
-when  "OER_DESC"  <>''  then 'External Ocular Examination'
-when "RSLBR_DESC"  <>'' then 'Routine Slit Lamp Biomicroscopy'
-when  "FUND_REDESC" <>'' then 'Direct/Indirect Fundoscopy'
-end as "Examination"
-from
-tas120_201."OPE") ope_201
+SLBL_DESC_1 :: text as "SLBL_DESC",
+RSLBR_DESC_1:: text as "RSLBR_DESC",
+Examination :: text as "Examination"
+from tas120_201."OPE"
+cross  join lateral (values  
+							('External Ocular Examination', "OEL_DESC","OER_DESC"),
+					 		 ('Routine Slit Lamp Biomicroscopy',"RSLBL_DESC","RSLBR_DESC"),
+					 		 ('Direct/Indirect Fundoscopy',"FUND_LEDESC","FUND_REDESC")
+					 )t (Examination,SLBL_DESC_1,RSLBR_DESC_1)) ope_201
 
 union all
 
-select
+select 	
 op."project" "Project",
 op."SiteNumber",
 op."Site",
 op."Subject",
 op."RecordId",
 v."OPHTPERF" as "OEPERF",
-op."FolderName" ,
+op."InstanceName" ,
 "OPEDAT"::date "OEDAT" ,
 null as "OEABN",
 "Examination",
-case when "Examination" = 'External Ocular Examination' then "OPELTABN"
-when "Examination" = 'Routine Slit Lamp Biomicroscopy' then "OPELTABN1"
-when "Examination" = 'Direct/Indirect Fundoscopy' then "OPEFUNLSPEC1"
-end as "Result_Left_Eye",
+"SLBL_DESC",
+"RSLBR_DESC"
 
-case when "Examination" = 'External Ocular Examination' then "OPERTABN"
-when "Examination" = 'Routine Slit Lamp Biomicroscopy' then "OPERTABN1"
-when "Examination" = 'Direct/Indirect Fundoscopy' then "OPEFUNSPEC"
-end as "Result_Right_Eye"
+
 
 from
 (
-select "project",
+select  distinct "project",
 "SiteNumber",
 "Site",
 "Subject",
 "RecordId",
-"FolderName" ,
-"InstanceName",
+"InstanceName" ,
 "OPEDAT" ,
 "OPELTABN",
 "OPELTABN1",
@@ -99,15 +85,15 @@ select "project",
 "OPERTABN",
 "OPERTABN1",
 "OPEFUNSPEC",
-case when "OPELTABN" <> '' then 'External Ocular Examination'
-when "OPELTABN1" <> '' then 'Routine Slit Lamp Biomicroscopy'
-when "OPEFUNLSPEC1" <> '' then 'Direct/Indirect Fundoscopy'
-when "OPERTABN" <> '' then 'External Ocular Examination'
-when "OPERTABN1" <> '' then 'Routine Slit Lamp Biomicroscopy'
-when "OPEFUNSPEC" <>'' then 'Direct/Indirect Fundoscopy'
-end as "Examination"
+SLBL_DESC_1 :: text as "SLBL_DESC",
+RSLBR_DESC_1:: text as "RSLBR_DESC",
+Examination :: text as "Examination"
 from tas120_202."OPE"
-) op
+cross  join lateral (values  
+							('External Ocular Examination', "OPELTABN","OPERTABN"),
+					 		 ('Routine Slit Lamp Biomicroscopy',"OPELTABN1","OPERTABN1"),
+					 		 ('Direct/Indirect Fundoscopy',"OPEFUNLSPEC1","OPEFUNSPEC")
+					 )t (Examination,SLBL_DESC_1,RSLBR_DESC_1)) op
 left join
 tas120_202."VISIT" v on
 op."project" = v."project" and
@@ -124,31 +110,24 @@ concat("project",substring("SiteNumber",position('_' in "SiteNumber"))) as "Site
 "Subject",
 "RecordId",
 "OPEPYN" as "OEPERF",
-"FolderName" ,
+"InstanceName" ,
 "OPEDAT"::date "OEDAT" ,
 "OPEYN" as "OEABN",
 "Examination",
-case when "Examination" = 'External Ocular Examination' then "OPELEDS"
-when  "Examination" = 'Routine Slit Lamp Biomicroscopy' then "OPELEABN"
-when "Examination" = 'Direct/Indirect Fundoscopy' then "OPELEFSDES"
-end as "Result_Left_Eye",
-
-case when "Examination" = 'External Ocular Examination' then "OPEREDS"
-when "Examination" = 'Routine Slit Lamp Biomicroscopy' then "OPEREABN"
-when "Examination" = 'Direct/Indirect Fundoscopy' then "OPEREFSDES"
-end as "Result_Right_Eye"
+"SLBL_DESC",
+"RSLBR_DESC"
 
 
 from
 (
-select
+select distinct
 "project",
 "SiteNumber",
 "Site",
 "Subject",
 "RecordId",
 "OPEPYN" ,
-"FolderName" ,
+"InstanceName" ,
 "OPEDAT",
 "OPEYN",
 "OPELEDS",
@@ -157,14 +136,16 @@ select
 "OPEREDS",
 "OPEREABN",
 "OPEREFSDES",
-case when "OPELEDS" <>'' then 'External Ocular Examination'
-when "OPELEABN" <>'' then 'Routine Slit Lamp Biomicroscopy'
-when "OPELEFSDES" <>'' then 'Direct/Indirect Fundoscopy'
-when "OPEREDS" <>'' then 'External Ocular Examination'
-when "OPEREABN"<>'' then 'Routine Slit Lamp Biomicroscopy'
-when "OPEREFSDES" <>'' then 'Direct/Indirect Fundoscopy'
-end as "Examination"
-from tas120_203."OPE") o
+SLBL_DESC_1 :: text as "SLBL_DESC",
+RSLBR_DESC_1:: text as "RSLBR_DESC",
+Examination :: text as "Examination"
+from tas120_203."OPE"
+cross  join lateral (values  
+							('External Ocular Examination',"OPELEDS","OPEREDS"),
+					 		 ('Routine Slit Lamp Biomicroscopy',"OPELEABN","OPEREABN"),
+					 		 ('Direct/Indirect Fundoscopy',"OPELEFSDES","OPEREFSDES")
+					 )t (Examination,SLBL_DESC_1,RSLBR_DESC_1)
+)ope_120_203
 
 union all
 
@@ -175,46 +156,40 @@ op."Site",
 op."Subject",
 op."RecordId",
 v."OPEPERF" as  "OEPERF",
-op."FolderName" ,
+op."InstanceName" ,
 "OEDAT"::date "OEDAT" ,
 "OEABN" as "OEABN",
 "Examination",
-case when "Examination" = 'External Ocular Examination' then "OEL_DESC"
-when "Examination" = 'Routine Slit Lamp Biomicroscopy' then "RSLBL_DESC"
-when "Examination" = 'Direct/Indirect Fundoscopy' then "FUND_LEDESC"
-end as "Result_Left_eye",
+"SLBL_DESC",
+"RSLBR_DESC"
 
-case when "Examination" = 'External Ocular Examination' then "OER_DESC"
-when  "Examination" = 'Routine Slit Lamp Biomicroscopy' then "RSLBR_DESC"
-when  "Examination" = 'Direct/Indirect Fundoscopy' then "FUND_REDESC"
-end as "Result_Right_Eye"
 
 from
 (
-select "project",
+select DISTINCT
+"project",
 "SiteNumber",
 "Site",
 "Subject",
 "RecordId",
-"FolderName" ,
-"InstanceName",
+"InstanceName" ,
 "OEDAT"::date  ,
 "OEABN",
 "OEL_DESC",
 "RSLBL_DESC",
 "FUND_LEDESC",
 "OER_DESC",
-"RSLBR_DESC",
+--"RSLBR_DESC",
 "FUND_REDESC",
-
-case when "OEL_DESC" <>'' then 'External Ocular Examination'
-when "RSLBL_DESC"<>'' then 'Routine Slit Lamp Biomicroscopy'
-when "FUND_LEDESC"<>'' then 'Direct/Indirect Fundoscopy'
-when "OER_DESC" <>'' then  'External Ocular Examination'
-when "RSLBR_DESC" <>'' then 'Routine Slit Lamp Biomicroscopy'
-when "FUND_REDESC" <>''then 'Direct/Indirect Fundoscopy'
-end as "Examination"
+SLBL_DESC_1 :: text as "SLBL_DESC",
+RSLBR_DESC_1:: text as "RSLBR_DESC",
+Examination :: text as "Examination"
 from tas120_204."OPE"
+cross  join lateral (values  
+							('External Ocular Examination',"OEL_DESC","OER_DESC"),
+					 		 ('Routine Slit Lamp Biomicroscopy',"RSLBL_DESC","RSLBR_DESC"),
+					 		 ('Direct/Indirect Fundoscopy',"FUND_LEDESC","FUND_REDESC")
+					 )t (Examination,SLBL_DESC_1,RSLBR_DESC_1)
 ) op
 left join
 tas120_204."VISIT" v on
@@ -233,12 +208,13 @@ op."Site",
 op."Subject",
 op."RecordId",
 v."OPEPERF" as "OEPERF",
-op."FolderName" ,
-"OEDAT"::date "OEDAT" ,
+op."InstanceName" ,
+"OEDAT"::date "OEDAT",
 null as "OEABN",
-null as "Examination",
-null as "Result_Left_eye",
-null as "Result_Right_Eye"
+op."OETEST" as "Examination",
+op."OECOMML" as "SLBL_DESC",
+op."OECOMMR" as "RSLBR_DESC"
+
 from tas2940_101."OPE" op
 left join
 tas2940_101."VISIT" v on
@@ -254,13 +230,17 @@ oe."Site",
 oe."Subject",
 oe."RecordId",
 oe."OEPERF",
-oe."FolderName",
+oe."InstanceName",
 oe."OEDAT" ,
 oe."OEABN",
-oe."Result_Left_Eye",
-oe."Result_Right_Eye",
-(oe."Project"||'~'||oe."SiteNumber"||'~'||oe."Site"||'~'||oe."Subject"||'~'||oe."FolderName"||'~'||oe."RecordId") as objectuniquekey
+oe."SLBL_DESC",
+oe."RSLBR_DESC",
+oe."Examination",
+(oe."Project"||'~'||oe."SiteNumber"||'~'||oe."Site"||'~'||oe."Subject"||'~'||oe."InstanceName"||'~'||oe."RecordId") as objectuniquekey
 from oe_data oe;
+
+
+
 
 
 
