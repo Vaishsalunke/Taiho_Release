d55d4e7b1253ffc6e36916dda28007d0a70ea8b4
Prateek Dubey <prateek.dubey@saama.com>
2022-09-06 17:12:01 +0530

    Removed New Study

--- Taiho_New/ccdm/global/cleanup/droptmpobjects.sql
+++
@@ -1,6 +0,0 @@
---DROP TABLE IF EXISTS dataentrydatetmp;
---DROP TABLE IF EXISTS fielddatatmp;
---DROP TABLE IF EXISTS getdataentrydate;
-DROP TABLE IF EXISTS getfielddata;
---DROP TABLE IF EXISTS getsdvdate;
-
